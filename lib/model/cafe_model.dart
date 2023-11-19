77bbbbc7c537686001fa87004a260755ac18df9e
Phillip Jhon Lumod <44081207+philliplumod@users.noreply.github.com>
2023-11-19 14:30:37 +0800

    reset

--- lib/model/cafe_model.dart
+++
@@ -1,8 +0,0 @@
-class CafeModel {
-  final String? id;
-  final String cafe;
-  final String city;
-
-  CafeModel(this.id, this.cafe, this.city);
-}
-factory
--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -12,42 +12,13 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Cloud Cafe'),
+        centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.black,
-        title: const Text('Cloud Cafe'),
-        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const Expanded(
-                    child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Cafe Name'))),
-                const SizedBox(width: 16.0),
-                const Expanded(
-                    child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Cafe Location'))),
-                const SizedBox(width: 8.0),
-                ElevatedButton(onPressed: () {}, child: const Text('Add Cafe'))
-              ],
-            ),
-
-            // fetch cafes from firestore and display them here
-            // if there are no cafes, display a message
-            ListView.builder(itemBuilder: (context, index) {
-              return ListTile();
-            })
-          ],
-        ),
-      ),
+      ,
     );
   }
 }
